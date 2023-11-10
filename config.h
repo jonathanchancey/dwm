7963fc6e36c0a8f1da93ed065bd6e13ab6b40569
Jonathan Chancey <jonathan@chancey.dev>
2023-11-10 04:26:27 -0800

    reorg patches, small config updates

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 6;        /* gaps between windows */
+static const unsigned int gappx     = 12;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -62,15 +62,14 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
-static const char *compilecmd[]  = { "cd ~/git/dwm && make && sudo make install", NULL };
-// static const char *roficmd[]  = { "rofi ", "-show", "run", NULL };
 static const char *killcmd[]  = { "pkill", "dwm", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	// { MODKEY,                       XK_space,  spawn,          {.v = roficmd } },
 	{ MODKEY,		                  	XK_space,	 spawn,          SHCMD("rofi -show drun") },
-	{ MODKEY|ShiftMask,	           	XK_c,   	 spawn,          {.v = compilecmd } },
+	{ MODKEY|ShiftMask,	           	XK_c,   	 spawn,          SHCMD("$HOME/scripts/compile-dwm.sh") },
+	{ MODKEY|ShiftMask,	           	XK_s,   	 spawn,          SHCMD("flameshot gui") },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_q,      spawn,          {.v = killcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
