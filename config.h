6bacd552d6b72dae915f95a512ab9a758e63b024
Jonathan Chancey <jonathan@chancey.dev>
2023-11-17 13:41:07 -0800

    disable some stacking conflicts

--- config.h
+++ config.h
@@ -256,7 +256,7 @@ static const Layout layouts[] = {
 #define STACKKEYS(MOD,ACTION) \
 	{ MOD, XK_j,     ACTION##stack, {.i = INC(+1) } }, \
 	{ MOD, XK_k,     ACTION##stack, {.i = INC(-1) } }, \
-	{ MOD, XK_s,     ACTION##stack, {.i = PREVSEL } }, \
+	// { MOD, XK_s,     ACTION##stack, {.i = PREVSEL } }, \
 	{ MOD, XK_w,     ACTION##stack, {.i = 0 } }, \
 	{ MOD, XK_e,     ACTION##stack, {.i = 1 } }, \
 	{ MOD, XK_a,     ACTION##stack, {.i = 2 } }, \
